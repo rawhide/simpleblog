3c5ad44c85dc8d94bb00e0d2fe07dc48ba30361f
michigoe <michigoe@efourtion.jp>
2013-12-22 15:38:19 +0900

    コメント途中

---
+++ app/controllers/articles/comment_controller.rb
@@ -0,0 +1,9 @@
+class Articles::CommentController < ApplicationController
+  def new
+  end
+  def create
+    binding.pry
+    @article = Article.find params[:article_id]
+    @comment = @article.comments.build
+  end
+end
