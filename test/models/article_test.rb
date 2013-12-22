f13409d2a899002d52053999d93a78745d769c29
Syo Suzuki <shou.suzuki0@gmail.com>
2013-12-22 15:24:25 +0900

    サンプル:articleとcommentの関連をテスト

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "関連のテスト" do
+    article = FactoryGirl.create :article, :has_comments
+    assert_equal 1, article.comments.count
+  end
 end
