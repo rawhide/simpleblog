fa25ab0da9a19306555b6b12695d33d9819a3de7
坂本 <sakamoto@MacBook-Air.local>
2013-12-22 15:09:35 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.destroy_all
+Comment.destroy_all
+[
+  {user_id: 1, title: "テスト1", body: "ほげほげほげ", pv: 1},
+  {user_id: 1, title: "テスト1", body: "ほげほげほげ", pv: 1},
+  {user_id: 1, title: "テスト1", body: "ほげほげほげ", pv: 1},
+  {user_id: 1, title: "テスト1", body: "ほげほげほげ", pv: 1}
+].each_with_index do |article, i|
+  a = Article.create article
+  i.times do
+    Comment.create({article_id: a.id, name: "ほげお#{i}", body: "コメント#{i}"})
+  end
+end
+
