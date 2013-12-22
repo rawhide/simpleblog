bb0c04d428449163dfee8344f72fbf1b01aa2a4d
ohta <ohta@raw-hide.co.jp>
2013-12-22 15:21:14 +0900

    test matigatta

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
   test ".read!でPvがインクリメントされること" do
-    subject = FactoryGirl :article
+    subject = FactoryGirl.create :article
     assert_equal 0, subject
 
     subject.read!
